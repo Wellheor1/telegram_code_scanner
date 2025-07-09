2e05bd047601916edae2a1b3569c32f1d4869b9d
well <Well.heor@yandex.ru>
2025-07-09 22:23:14 +0800

    telegram miniapp

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 
 app = FastAPI()
+static_path = Path(__file__).parent / "static"
+print(static_path)
+app.mount("/static", StaticFiles(directory=static_path), name="static")
 
 
 @app.get("/", tags=["Приложение"])
 async def root():
-    return {"ok": True, "message": "hello world"}
+    return FileResponse(static_path / "index.html")
